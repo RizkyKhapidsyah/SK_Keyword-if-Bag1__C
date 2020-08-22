6937cf6f278371205e01224d8304105e6d1eabbc
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-22 17:49:18 +0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a;
+
+	printf("Type an integer: ");
+	scanf_s("%d",&a);
+
+	printf("You typed %d.\n",a);
+
+	if( a > 10 )
+		printf("%d is greater than 10.\n",a);
+
+	_getch();
+	return(0);
+}
